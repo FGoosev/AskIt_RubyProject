dd14fe937c41b946224cc0734d9237deb945fb60
fgoosev <fgoosev@gmail.com>
2022-03-16 16:57:45 +0400

    Добавлена регистрация и авторизация. Застилизовано

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bcrypt', '~> 3.1.7'
 gem 'faker', '~> 2'
 gem 'kaminari', '~> 1.2'
+gem 'devise', '~> 4.8.1'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resource :session, only: %i[new create destroy]
-  resources :users, only: %i[new create]
+  devise_for :users
   resources :questions do
     resources :ans
   end
